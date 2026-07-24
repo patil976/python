de5fbdd86e6f6a2818b426b8105d61dc9a996254
patil976 <devpatil4232@gmail.com>
2026-07-24 09:56:24 +0000

    add 3 python program

---
+++ forloop1.py
@@ -0,0 +1,24 @@
+#natural no up to n
+'''n = int(input("Enter n: "))
+for i in range(1, n + 1):
+    print(i, end=" ")'''
+
+#design of a b c 
+'''repeat = 3
+i = 0
+while i < repeat:
+    print("A B C", end="  ")
+    i += 1'''
+
+
+#even no up to n 
+'''n = int(input("Enter n: "))
+for i in range(2, n + 1, 2):
+    print(i, end=" ")'''
+
+
+#odd no up to n
+n = int(input("Enter n: "))
+for i in range(1, n + 1, 2):
+    print(i, end=" ")
+
---
+++ whileloop1.py
@@ -0,0 +1,40 @@
+#natural no
+'''n = int(input("Enter n: "))
+i = 1
+while i <= n:
+    print(i, end=" ")
+   i += 1'''
+
+#even no up to n
+'''n = int(input("Enter n: "))
+i = 2
+while i <= n:
+    print(i, end=" ")
+    i += 2 '''
+
+#odd no up to n
+
+'''n = int(input("Enter n: "))
+i = 1
+while i <= n:
+    print(i, end=" ")
+    i +=2'''
+
+#sum of odd no
+'''n = int(input("Enter n: "))
+total_sum = 0
+i = 1
+while i <= n:
+    total_sum += i
+    i += 2  
+print("Sum of odd numbers:", total_sum)'''
+
+#sum of even no
+n = int(input("Enter n: "))
+total_sum = 0
+i = 2
+while i <= n:
+    total_sum += i
+    i += 2  
+print("Sum of even numbers:", total_sum)
+
