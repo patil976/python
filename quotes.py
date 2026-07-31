80bf997842c85e4a1e78634ae182243a7dcc1e8b
patil976 <devpatil4232@gmail.com>
2026-07-31 15:27:54 +0530

    Add files via upload

---
+++ quotes.py
@@ -0,0 +1,11 @@
+'''str1='hello,world!'
+print(str1)'''
+
+'''str2="welcome to python"
+print(str2)'''
+
+#str3=''' Python is a high-level, interpreted programming language widely known for its simplicity, readability, and versatility. Created by Guido van Rossum and released in 1991, its straightforward syntax closely resembles natural English, making it an excellent choice for beginners and experienced developers alike. Python supports multiple programming paradigms, including object-oriented, procedural, and functional programming.'''
+#print(str3)
+
+str4=""" recognized for its simple, English-like syntax that prioritizes code readability and developer efficiency. Maintained by the Python Software Foundation, it functions as an open-source, versatile tool widely adopted across data science, web development, and artificial intelligence."""
+print(str4)
